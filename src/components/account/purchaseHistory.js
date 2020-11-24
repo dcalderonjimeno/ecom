3ef2c13409813e0fb71dc7d484c1739974610371
dcalderonjimeno <dcalderonjimeno@gmail.com>
2020-11-24 21:31:54 +0100

    built base structure for account components

---
+++ src/components/account/purchaseHistory.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class PurchaseHistory extends Component {
+    render() {
+        return (
+            <div>
+                purchase info
+            </div>
+        )
+    }
+}
+export default PurchaseHistory;
--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -4,26 +4,8 @@ import {
 } from '../actions/types';
 
 const INITIAL_STATE = {
-    headerLinks: [
-        {
-            title: 'helloooo'
-
-        },
-        {
-            title:'HELLOOOO'
-        }
-    ],
-    navbarLinks: [
-        {
-            title: 'account',
-            active: true
-
-        },
-        {
-            title:'purchases',
-            active: false
-        }
-    ]
+    headerLinks: [],
+    navbarLinks: []
 }
 
 export default function(state = INITIAL_STATE, action) {
