233d743558efcea94b9cbd01f16a032d64e7be87
dcalderonjimeno <dcalderonjimeno@gmail.com>
2020-11-24 13:49:44 +0100

    styled header and navbar components

--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -15,11 +15,13 @@ const INITIAL_STATE = {
     ],
     navbarLinks: [
         {
-            title: 'account'
+            title: 'account',
+            active: true
 
         },
         {
-            title:'purchases'
+            title:'purchases',
+            active: false
         }
     ]
 }
