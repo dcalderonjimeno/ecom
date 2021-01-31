0539aafcbdf00d246cc01d2378c9df27742afe37
dcalderonjimeno <dcalderonjimeno@gmail.com>
2021-01-31 13:03:02 +0100

    finished account information form

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -3,11 +3,10 @@ import React, { Component } from 'react';
 export class FormInput extends Component {
     render() {
         const { className, title, type, placeholder, input } = this.props;
-
         return (
             <div className={`${className} form-input`}>
                 <label className='form-input__label'>{title}</label>
-                <input className='form-input__input' type={type} placeholder={placeholder} {...input}/>
+                <input className='form-input__input' type={type} placeholder={placeholder} {...input} />
             </div>
         )
     }
@@ -16,14 +15,12 @@ export class FormInput extends Component {
 export class FormButton extends Component {
     render() {
         const { className, title, type, onClick, input, short } = this.props;
-
         return (
-            <div className={`${className} form-button ${short ? 'form-button__gray-button' : ''}`}>
-                <button 
-                    className={`form-button__button`}
-                    type={type}
-                    {...input}
-                    onClick={onClick}
+            <div className={`${className} form-button`}>
+                <button className={`form-button__button ${short ? 'form-button__gray-button' : ''}`}
+                        type={type}
+                        {...input}
+                        onClick={onClick}
                 >
                 {title}
                 </button>
@@ -32,18 +29,17 @@ export class FormButton extends Component {
     }
 }
 
+
 export class LongGrayButton extends Component {
     render() {
         const { className, labelTitle, title, type, onClick, input } = this.props;
-
         return (
             <div className={`${className} form-button-long-gray`}>
                 <label className='form-button-long-gray__label'>{labelTitle}</label>
-                <button 
-                    className={`form-button-long-gray__button`}
-                    type={type}
-                    {...input}
-                    onClick={onClick}
+                <button className={`form-button-long-gray__button`}
+                        type={type}
+                        {...input}
+                        onClick={onClick}
                 >
                 {title}
                 </button>
